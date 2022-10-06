8134a68e44f1d192602e438b6a6786de5ea0594a
SreekanthSreedharan22 <sreekanth.kochi22@gmail.com>
2022-10-06 12:45:35 +0530

    commented code for checking some headers in the ResponseMessagesHandler

--- WikicChangeClient/src/main/java/com/learning/kafkaintegration/consumer/ResponseMessagesHandler.java
+++ WikicChangeClient/src/main/java/com/learning/kafkaintegration/consumer/ResponseMessagesHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,11 @@ public class ResponseMessagesHandler {
     @KafkaListener(topics = {"#{'${kafka.wikichanges.consumer.topic}'.split(',')}"},
     containerFactory = "responseMessageKafkaListenerContainerFactory")
     public void receiveResponseMessage(@Payload ResponseMessage responseMessage,
+//                                       @Header(value = KafkaHeaders.ORIGINAL_TIMESTAMP, required = false) String originalTimeStamp,
+//                                       @Header(value = KafkaHeaders.TIMESTAMP, required = false) String timeStamp,
                                        @Header(value = "is-last-record", required = false) String isLastRecord) {
-        log.info("responseMessage received: {}", responseMessage);
+        log.info("responseMessage received, responseMessage: {}", responseMessage);
+//        log.info("responseMessage received, originalTimeStamp: {}, timeStamp: {}", originalTimeStamp, timeStamp);
 
         if(StringUtils.isNotBlank(isLastRecord) && isLastRecord.equals("true")) {
             log.info(">>>> last message received for correlationId: {}", responseMessage.getCorrelationId());
